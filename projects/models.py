7188a926eaeba7b1d9eceef6fd6487aabe39cfa9
Uttam <utam.shrestha65@gmail.com>
2023-03-22 19:14:55 +0545

    templates added and migrated

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=100)
--- projects/urls.py
+++ projects/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from projects import views
 
 urlpatterns =[
-    path('/', views.uttam, name = 'uttam')
+    path('', views.uttam, name = 'uttam')
 ]
